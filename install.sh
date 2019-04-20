fdfb5cf175530614f034909edd7b59000f5f5032
HuangYong <newfriday@126.com>
2019-04-20 21:33:32 +0800

    centos init
    1. disable selinux
    2. load iptable rules for NAT
    
    Signed-off-by: HuangYong <newfriday@126.com>

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+ARCH=""
+if [ $# -eq 1 ]; then
+	if [ "X$1" == "Xcentos" -o "X$1" == "Xubuntu" ]; then
+		ARCH=$1
+	else
+		echo "parameter error"
+	fi
+fi
 
 ROOT=$(cd "$(dirname "$0")"; pwd)
 PLUGINS="$ROOT/plugins"
 
+[ -d $ROOT/plugins ] && rm -rf $ROOT/plugins
 tar -xf $ROOT/plugins.tar.gz
 
 function load_vim()
@@ -16,35 +25,37 @@ function load_tmux()
 	[ -f /root/.tmux.conf ] || cp "$ROOT/tmux.conf" /root/.tmux.conf
 }
 
+function config_centos()
+{
+	cp -f "$ROOT/iptables.rules" /etc/sysconfig/iptables.rules
+	cat /etc/rc.d/rc.local | grep "iptables-restore < /etc/sysconfig/iptables.rules" > /dev/null
+	if [ $? -ne 0 ]; then
+		[ -f /etc/rc.d/rc.local ] && \
+		echo "iptables-restore < /etc/sysconfig/iptables.rules" >> /etc/rc.d/rc.local && \
+		echo "config iptable rules done"
+	fi
+
+	[ -f /etc/sysconfig/selinux ] && \
+	sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/sysconfig/selinux && \
+	echo "config selinux done"
+
+	return 0
+}
+
+function config_ubuntu()
+{
+	return 0
+}
+
 load_vim
 load_tmux
 
-echo "alias jcode='cd /home/code'
-alias jscripts='cd /home/code/scripts'
-alias jlibvirt='cd /home/code/libvirt/libvirt-4.6.0'
-alias jqemu='cd /home/code/official_qemu'
-alias jbochs='cd /home/code/bochs/'
-alias jseabios='cd /home/code/seabios/seabios-1.11.1'
-alias jwork='cd /home/work'
-alias sbochs='/usr/local/bin/bochs -q'
-alias vml='virsh list'
-alias vmsc7='virsh start CentOS_1804'
-alias vmdc7='virsh destroy CentOS_1804'
-alias vmec7='ssh centos02'
-alias vmsu18='virsh start Ubuntu_1810'
-alias vmeu18='ssh ubuntu03'
-alias vmdu18='virsh destroy Ubuntu_1810'
-alias joch1='cd /home/work/orange/source/chapter1'
-alias joch2='cd /home/work/orange/source/chapter2'
-alias joch3='cd /home/work/orange/source/chapter3'
-alias joch4='cd /home/work/orange/source/chapter4'
-alias joch5='cd /home/work/orange/source/chapter5'
-alias joch6='cd /home/work/orange/source/chapter6'
-alias joch7='cd /home/work/orange/source/chapter7'
-alias joch8='cd /home/work/orange/source/chapter8'
-alias joch9='cd /home/work/orange/source/chapter9'
-alias joch10='cd /home/work/orange/source/chapter10'
-alias joch11='cd /home/work/orange/source/chapter11'
-" >> /root/.bashrc
+if [ "X$ARCH" == "Xcentos" ]; then
+	echo "config centos ..."
+	config_centos
+fi
 
-source /root/.bashrc
+if [ "X$ARCH" == "Xubuntu" ]; then
+	echo "config ubuntu ..."
+	config_ubuntu
+fi
